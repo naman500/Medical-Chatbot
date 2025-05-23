ea02261dcbe27750058faa16be5ed3dd10f9b17e
Naman Soni <naman21csu438@ncuindia.edu>
2025-05-24 02:20:25 +0530

    Get embedding model and done with store embeddings in FAISS

--- create_memory_for_llm.py
+++ create_memory_for_llm.py
@@ -1,6 +1,5 @@
 #  Step 1: Load Raw Pdf (S)
 
-
 from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
@@ -26,5 +25,14 @@ text_chunck=create_chunks(extrected_text=documents)
 print("Lenght of text chunks",len(text_chunck))
 
 # Step 3 : Create Vector Embeddings
+def get_embedding_model():
+    embedding_node = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return embedding_node
+
+embedding_model=get_embedding_model()
+
 # Step 4 :Store embeddings in FAISS
+DB_FAISS_PATH="vectorstore/db_faiss"
+db=FAISS.from_documents(text_chunck,embedding_model)
+db.save_local(DB_FAISS_PATH)
   
